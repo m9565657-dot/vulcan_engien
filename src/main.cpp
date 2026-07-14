5a0cac8bbbd5beac711d9abf4880310bfce1b8c9
maksim <m9565657@gmail.com>
2026-07-15 00:35:42 +0300

    find GPU

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include "windows_layer/windows_manager.h"
 #include <vulkan/vulkan.h>
 #include <stdexcept>
+#include <vector>
 
 int main(){
     Window myWindow(680, 840, "HELLOW_WORLD");
+
+    // Step 1: Vulkan Instance 
 
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -21,7 +24,19 @@ int main(){
     if(result != VK_SUCCESS){
         throw std::runtime_error("FAILED TO CREATE VULCAN INSTANCE!");
     }
-    
+
+    // Step 2: change a videocard 
+
+    uint32_t deviceCount = 0; 
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+    std::vector<VkPhysicalDevice> gpu_list(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, gpu_list.data());
+
+    VkPhysicalDevice physicalDevice = gpu_list[0];
+
+    // LOOP
+
     while(!myWindow.ShouldClose()){
         myWindow.PollEvents();
     }
